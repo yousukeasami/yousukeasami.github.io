e43f34918a1aeba7be231980ef83c25c3d9dd355
Yousuke Asami <asaminews@icloud.com>
2016-02-15 18:53:15 +0900

    no message

---
+++ biznews/menu.js
@@ -0,0 +1,14 @@
+$(function(){
+    var box   = $("#header");
+    var boxTop = box.offset().top;
+
+    $(window).scroll(function () {
+        if($(window).scrollTop() > boxTop) {
+            box.addClass("fixed");
+            $("body").css("margin-top","100px");
+        } else {
+            box.removeClass("fixed");
+            $("body").css("margin-top","0px");
+        }
+    });
+});
---
+++ biznews/slide.js
@@ -0,0 +1,22 @@
+
+<!--
+
+
+  $(document).ready( function() {
+
+        $( '#slide-img' ) . cycle( {
+            fx: 'scrollLeft',
+            speed: 2000, 
+            timeout: 3000,
+            sync: 1,
+            next: 'next',
+            prev: 'prev'
+        } );
+
+       $('#slide-wrap') . hover(
+            function() { jQuery( '#nav' ) . fadeIn(); },
+            function() { jQuery( '#nav' ) . fadeOut(); }
+        );
+    });
+
+// -->
