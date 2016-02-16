e9b13655a07df7f199a82379c2257aa27b0b169f
Yousuke Asami <asaminews@icloud.com>
2016-02-16 16:57:36 +0900

    no message

---
+++ biznews/slide.js
@@ -0,0 +1,27 @@
+
+<!--
+
+
+  $(document).ready( function() {
+
+        $( '#slide-img' ) . cycle( {
+            fx: 'scrollLeft',
+            speed: 2000, 
+            timeout: 0,
+            sync: 1,
+            next: 'next',
+            prev: 'prev',
+            
+        } );
+
+       $('#slide-wraper') . hover(
+            function() { $( '#nav' ) . fadeIn(); },
+            function() { $( '#nav' ) . fadeOut(); }
+        );
+
+
+
+
+    });
+
+// -->
