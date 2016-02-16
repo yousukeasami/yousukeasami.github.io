9f117c4c2540941698ce95131fd54461975b9a91
Yousuke Asami <asaminews@icloud.com>
2016-02-16 17:15:57 +0900

    no message

--- biznews/menu.js
+++ biznews/menu.js
@@ -5,10 +5,37 @@ $(function(){
     $(window).scroll(function () {
         if($(window).scrollTop() > boxTop) {
             box.addClass("fixed");
-            $("body").css("margin-top","100px");
+            $("body").css("margin-top","0px");
         } else {
             box.removeClass("fixed");
             $("body").css("margin-top","0px");
         }
     });
 });
+
+
+$(function() {
+
+    $('.showhide').css('display','none');
+
+    $('#menu li').click(function() {
+        var tabnum = $('#menu li').index(this);
+        $('.content .showhide').css('display','none');
+        $('.content .showhide').eq(tabnum).css('display','block');
+        $('#menu li').removeClass('active');
+        $(this).addClass('active')
+    });
+
+    $('#menu>li').tabs({
+         fx: { opacity: 'toggle', duration: 'slow'  } 
+     });
+
+});
+
+
+
+
+
+
+
+
